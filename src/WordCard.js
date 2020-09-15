eb3b48963ba485b733ac2135d206a4d9598fe71c
thanida-pai <s6135512015@phuket.psu.ac.th>
2020-09-15 16:37:07 +0700

    second commit

--- src/WordCard.js
+++ src/WordCard.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import CharacterCard from './CharacterCard';
 import _ from 'lodash';
-// import { useState, useEffect } from '../node_modules/react/cjs/react.production.min';
+const refreshPage = () => window.location.reload(false);
 
 const prepareStateFromWord = (given_word) => {
     let word = given_word.toUpperCase()
@@ -14,9 +14,7 @@ const prepareStateFromWord = (given_word) => {
         completed: false
     }
 }
-
 export default function WordCard(props) {
-    // const activationHandler = c => { console.log(`${c} has been activated.`) }
     const [state, setState] = useState({
         word: '',
         chars: '',
@@ -24,7 +22,6 @@ export default function WordCard(props) {
         guess: '',
         completed: false
     })
-
     useEffect(() => {
         let data = prepareStateFromWord(props.value);
         setState({
@@ -35,30 +32,30 @@ export default function WordCard(props) {
             guess: data.guess,
             completed: data.completed
         })
-    },[])
-
+    }, [])
     const activationHandler = (c) => {
         console.log(`${c} has been activated.`)
         let guess = state.guess + c
         setState({ ...state, guess })
         if (guess.length == state.word.length) {
             if (guess == state.word) {
-                console.log('yeah!')
+                console.log('Correct')
                 setState({ ...state, guess: '', completed: true })
             } else {
                 console.log('reset')
                 setState({ ...state, guess: '', attempt: state.attempt + 1 })
+                refreshPage()
+
             }
         }
     }
-
-
     return (
         <div>
-            {Array.from(state.chars).map((c, i) => <CharacterCard value={c} key={i} 
-            activationHandler={activationHandler} 
-            attempt={state.attempt}/>)}
+            {Array.from(state.chars).map((c, i) => <CharacterCard value={c} key={i}
+                activationHandler={activationHandler}
+                attempt={state.attempt} />)}
             {/* <CharacterCard value={c} key={i} activationHandler={activationHandler}/> */}
         </div>
     );
+    
 }
